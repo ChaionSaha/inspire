0130066abd6e5e69a8eda27457faf55a67fc9ee8
Chaion Saha <chaionsaha00@gmail.com>
2024-05-08 03:51:09 +0600

    changed the image sizes and updated ui of homepage

--- components/global/Layout.js
+++ components/global/Layout.js
@@ -1,7 +1,37 @@
+import SearchIcon from '@/assets/icons/SearchIcon';
+import ShoppingbagIcon from '@/assets/icons/ShoppingbagIcon';
+import UserIcon from '@/assets/icons/UserIcon';
+import whiteLogo from '@/assets/logo-white.png';
+import Image from "next/image";
 
 export default function Layout({children}) {
     return (
-        <div>
+        <div className="relative">
+            <div className="absolute py-5 text-white top-0 left-0 w-full bg-[rgba(255,255,255,0.04)] z-[100] border-b border-[rgba(255,255,255,0.19)]">
+                <div className="container mx-auto">
+                    <div className="flex justify-between">
+                        <div className="flex gap-x-5">
+                            <p>New Arrival</p>
+                            <p>Shop</p>
+                            <p>Collection</p>
+                        </div>
+                        <div className="relative w-28 h-10">
+                            <Image src={whiteLogo} fill alt="Inspire Logo"/>
+                        </div>
+                        <div className="flex gap-x-5">
+                            <button>
+                                <SearchIcon color='white' />
+                            </button>
+                            <button>
+                                <UserIcon color='white' />
+                            </button>
+                            <button >
+                                <ShoppingbagIcon color='white'/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             {children}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,27 @@
-import slider1 from '@/assets/slider-1.png';
-import slider2 from '@/assets/slider-2.png';
-import slider3 from '@/assets/slider-3.png';
 import AnimateLayout from "@/components/global/AnimateLayout";
 import Title from "@/components/global/Title";
+import { getDatabase } from '@/lib/mongoConnection';
 import Image from "next/image";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
 
-const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows:false
-};
 
-export default function Home() {
-  
+export default function Home({hero}) {
     return (
         <AnimateLayout>
             <Title title={'Home'}/>
-            <Slider {...settings} >
-                <div className="relative w-full h-[95vh] border" >
-                    <Image src={slider1} alt="slider-1" fill className=' object-cover' quality={100}/>
-                </div>
-                <div className="relative w-full h-[95vh]">
-                    <Image src={slider2} alt="slider-1" fill className=' object-cover' quality={100}/>
-                </div>
-                <div className="relative w-full h-[95vh]">
-                    <Image src={slider3} alt="slider-1" fill className=' object-cover' quality={100}/>
-                </div>
-            </Slider>
+            <div className="relative w-full h-[95vh]" >
+                <Image src={hero.img} alt={hero.title} fill className=' object-cover' quality={100}/>
+            </div>   
         </AnimateLayout>
     );
+}
+
+export async function getServerSideProps() {
+    const db = await getDatabase();
+    const homepageHero = await db.collection('homepage').find().project({ _id: 0 }).toArray();
+
+    return {
+        props: {
+            hero:homepageHero[0]
+        }
+    }
 }
