c01f917d1d969000d8b5515b4e614f409e82080e
Chaion Saha <chaionsaha00@gmail.com>
2024-05-26 20:51:30 +0600

    completed cart logic

--- components/global/Cart.js
+++ components/global/Cart.js
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 
 const Cart = ({cartOpen, setCartOpen}) => {
     const {cart} = useSelector(state => state.cart);
-    
-    
-    useEffect(() => {
-        console.log(cart)
-    },[cart])
-    
 
     return (
         <div className={`fixed bg-primary h-[100vh] flex flex-col overflow-y-auto w-[25%] z-[1000] top-0 right-0 duration-300 ${cartOpen ? 'translate-x-0' : 'translate-x-[100%]'}`}>
@@ -17,7 +10,7 @@ const Cart = ({cartOpen, setCartOpen}) => {
                 <p className='text-3xl font-medium text-white px-5'>Shopping Bag</p>
                 <button onClick={()=>setCartOpen(false)} className='text-white text-4xl'><i className='bi bi-x'></i></button>
             </div>
-            <div className="p-5 flex-grow">
+            <div className="p-5 flex-grow flex flex-col gap-10">
                 {
                     cart.length > 0 ?
                         cart.map((c, i) =>
--- components/global/CartItem.js
+++ components/global/CartItem.js
@@ -1,15 +1,32 @@
+import { decrementItem, incrementItem, removeFromCart } from "@/lib/store";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
 
-const CartItem = ({item}) => {
+const CartItem = ({ item }) => {
+    const dispatch = useDispatch();
+
     return (
         <div className='flex gap-x-5 text-white'>
-            <div className='relative w-20 h-20'>
+            <div className='relative w-[20%]'>
                 <Image src={item.images[0].img} fill alt={item.name} className="object-contain"/>
             </div>
-            <div>
-                <p className='text-lg font-medium'>{item.name}</p>
-                <p className='text-lg font-bold'>${item.price}</p>
+            <div className="flex-grow">
+                <p className='text-xl font-medium'>{item.name}</p>
+                <p className=' font-bold'>${item.price}</p>
+                <div className="flex mt-5 justify-between items-center w-full">
+                    <div className="flex items-center ">
+                        <p className="text-base-300">Quantity: </p>
+                        <div className="flex gap-x-2 items-center">
+                            <button className="bt bt-ghost btn-xs" onClick={()=>dispatch(decrementItem(item.id))}><i className="bi bi-dash text-lg"></i></button>
+                            <p className="text-white px-2" >{item.quantity}</p>
+                            <button className="bt bt-ghost btn-xs" onClick={()=>dispatch(incrementItem(item.id))}><i className="bi bi-plus text-lg"></i></button>
+                        </div>
+                    </div>
+                    <button className="underline" onClick={()=>{dispatch(removeFromCart(item.id))}}>Remove</button>
+                </div>
             </div>
+
+            
         </div>
     );
 }
