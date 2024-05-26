3fda6d0d14359f4d1e21392d9e069f179073c955
Chaion Saha <chaionsaha00@gmail.com>
2024-05-26 21:05:46 +0600

    cart completed

--- components/global/Cart.js
+++ components/global/Cart.js
@@ -1,16 +1,28 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 
 const Cart = ({cartOpen, setCartOpen}) => {
-    const {cart} = useSelector(state => state.cart);
+    const { cart } = useSelector(state => state.cart);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        let temp = 0;
+        cart.forEach(item => {
+            temp += item.price * item.quantity;
+        });
+        setTotal(temp);
+    },[cart])
 
     return (
-        <div className={`fixed bg-primary h-[100vh] flex flex-col overflow-y-auto w-[25%] z-[1000] top-0 right-0 duration-300 ${cartOpen ? 'translate-x-0' : 'translate-x-[100%]'}`}>
-            <div className="p-5 py-7 flex justify-between items-center">
+        <div className={`fixed bg-primary h-[100vh] flex flex-col  w-[25%] z-[1000] top-0 right-0 duration-300 ${cartOpen ? 'translate-x-0' : 'translate-x-[100%]'}`}>
+            <div className="px-5 py-7 flex justify-between items-center">
                 <p className='text-3xl font-medium text-white px-5'>Shopping Bag</p>
                 <button onClick={()=>setCartOpen(false)} className='text-white text-4xl'><i className='bi bi-x'></i></button>
             </div>
-            <div className="p-5 flex-grow flex flex-col gap-10">
+            
+
+            <div className="p-5 me-3 flex-grow flex flex-col gap-10 overflow-y-auto">
                 {
                     cart.length > 0 ?
                         cart.map((c, i) =>
@@ -19,6 +31,22 @@ const Cart = ({cartOpen, setCartOpen}) => {
                         <p className='text-lg text-center text-white'>No items in your cart</p>
                 }
             </div>
+
+            {
+                cart.length > 0 &&
+                <div className="border-t py-5">
+                    <div className="flex items-center justify-between text-white px-5">
+                        <div className="flex gap-x-5">
+                            <p>Subtotal</p>
+                            <p className="text-base-300">({cart.length} item)</p>
+                        </div>
+                        <p>${total}</p>
+                    </div>
+                    <div className="px-5 w-full mt-5">
+                        <button className="btn w-full bg-white text-black hover:text-white"> Review + Checkout</button>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
